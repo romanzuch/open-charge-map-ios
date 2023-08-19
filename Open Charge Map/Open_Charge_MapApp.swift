8b3ffe103f0b3f649d25c6a28d4a98b70d44c877
Roman <roman.zuchowski@gmail.com>
2023-08-19 14:21:06 +0200

    Initial Commit

---
+++ Open Charge Map/Open_Charge_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Open_Charge_MapApp.swift
+//  Open Charge Map
+//
+//  Created by Roman Zuchowski on 19.08.23.
+//
+
+import SwiftUI
+
+@main
+struct Open_Charge_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
