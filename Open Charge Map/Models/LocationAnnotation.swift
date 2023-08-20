527e685704c3863c2ec95df56970567c8e30349e
Roman <roman.zuchowski@gmail.com>
2023-08-20 14:01:59 +0200

    this will add more parameters to the location annotation

--- Open Charge Map/Models/LocationAnnotation.swift
+++ Open Charge Map/Models/LocationAnnotation.swift
@@ -7,15 +7,22 @@
 
 import Foundation
 import MapKit
+import SwiftUI
 
-class LocationAnnotation: NSObject {
+class LocationAnnotation: NSObject, MKAnnotation {
     var coordinate: CLLocationCoordinate2D
-    
-    init(for location: Location) {
+    var locationName: String?
+    var locationDescription: String?
+    var locationColor: Color?
+
+    init(for location: Location, locationDescription: String?) {
+
         self.coordinate = CLLocationCoordinate2D(
             latitude: CLLocationDegrees(location.properties.address.lat),
             longitude: CLLocationDegrees(location.properties.address.lng)
         )
-        super.init()
+        self.locationName = location.properties.address.title
+        self.locationDescription = locationDescription != nil ? locationDescription: location.properties.locationOperator.title
+        self.locationColor = location.properties.status.operational == true ? .green : .red
     }
 }
