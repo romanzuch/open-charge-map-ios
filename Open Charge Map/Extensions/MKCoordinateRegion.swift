8f650926b1ba927b7820cebf847994fd30144ec3
Roman <roman.zuchowski@gmail.com>
2023-08-21 19:36:28 +0200

    this will add more methods to the extension

--- Open Charge Map/Extensions/MKCoordinateRegion.swift
+++ Open Charge Map/Extensions/MKCoordinateRegion.swift
@@ -8,13 +8,23 @@
 import MapKit
 
 extension MKCoordinateRegion {
-    func isMoreThan50PercentOutside(of otherRegion: MKCoordinateRegion) -> Bool {
-        let latitudeDifference = abs(center.latitude - otherRegion.center.latitude)
-        let longitudeDifference = abs(center.longitude - otherRegion.center.longitude)
-        
-        let maxLatitudeSpan = (span.latitudeDelta + otherRegion.span.latitudeDelta) / 2
-        let maxLongitudeSpan = (span.longitudeDelta + otherRegion.span.longitudeDelta) / 2
-        
-        return latitudeDifference > maxLatitudeSpan * 0.5 || longitudeDifference > maxLongitudeSpan * 0.5
+    var maxLongitude: CLLocationDegrees {
+        center.longitude + span.longitudeDelta / 2
+    }
+
+    var minLongitude: CLLocationDegrees {
+        center.longitude - span.longitudeDelta / 2
+    }
+
+    var maxLatitude: CLLocationDegrees {
+        center.latitude + span.latitudeDelta / 2
+    }
+
+    var minLatitude: CLLocationDegrees {
+        center.latitude - span.latitudeDelta / 2
+    }
+
+    func contains(_ other: MKCoordinateRegion) -> Bool {
+        maxLongitude >= other.maxLongitude && minLongitude <= other.minLongitude && maxLatitude >= other.maxLatitude && minLatitude <= other.minLatitude
     }
 }
