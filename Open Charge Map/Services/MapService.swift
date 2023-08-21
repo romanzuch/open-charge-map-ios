3afd44e167c9c31f29f914d30f52aea1e596c5a8
Roman <roman.zuchowski@gmail.com>
2023-08-21 14:19:14 +0200

    this will refactor the service, again...

--- Open Charge Map/Services/MapService.swift
+++ Open Charge Map/Services/MapService.swift
@@ -11,29 +11,46 @@ import CoreLocation
 import SwiftUI
 
 protocol MapServices {
-    func calculateDiagonalKilometers(_ coordinateRegion: MKCoordinateRegion) -> Double
+    func calculateDiagonalKilometers(_ coordinateRegion: MKCoordinateRegion) -> String
     func getCoordinatesFromAddress(address: LocationAddress) -> CLLocationCoordinate2D
-    func getTintFromStatus(_ status: Status) -> Color
+    func getTintFromStatus(_ status: Status) -> UIColor
+    func getLocationAnnotations(for locations: [Location]) -> [LocationAnnotation]
+    func getPOIFilter() -> MKPointOfInterestFilter
 }
 
 class MapService: MapServices {
+    
     static let shared: MapService = MapService()
-    func calculateDiagonalKilometers(_ coordinateRegion: MKCoordinateRegion) -> Double {
+    
+    func calculateDiagonalKilometers(_ coordinateRegion: MKCoordinateRegion) -> String {
         let latDelta: Double = MKMetersPerMapPointAtLatitude(coordinateRegion.span.latitudeDelta)
         let lngDelta: Double = MKMetersPerMapPointAtLatitude(coordinateRegion.span.longitudeDelta)
-        return sqrt(latDelta * latDelta + lngDelta * lngDelta)
+        let distance: Double = sqrt(latDelta * latDelta + lngDelta * lngDelta)
+        let distanceString: String = String(format: "%.2f", distance).replacingOccurrences(of: ".", with: ",")
+        return distanceString
     }
+    
     func getCoordinatesFromAddress(address: LocationAddress) -> CLLocationCoordinate2D {
         return CLLocationCoordinate2D(latitude: CLLocationDegrees(address.lat), longitude: CLLocationDegrees(address.lng))
     }
-    func getTintFromStatus(_ status: Status) -> Color {
+    
+    func getTintFromStatus(_ status: Status) -> UIColor {
         switch status.operational {
             case true:
-                return Color.green
-            case false:
-                return Color.red
-            default:
-                return Color.gray
+                return UIColor(red: 34/255, green: 197/255, blue: 94/255, alpha: 1) // green
+            case false: 
+                return UIColor(red: 153/255, green: 27/255, blue: 27/255, alpha: 1) // red
         }
+    }
+    
+    func getLocationAnnotations(for locations: [Location]) -> [LocationAnnotation] {
+        return locations.map { location in
+            let annotation = LocationAnnotation(for: location, locationDescription: nil)
+            return annotation
+        }
+    }
+    
+    func getPOIFilter() -> MKPointOfInterestFilter {
+        return MKPointOfInterestFilter(including: [.bank, .airport, .atm, .bakery, .cafe, .carRental, .evCharger, .hospital, .hotel, .store, .restroom, .restaurant, .parking, .pharmacy, .police, .publicTransport])
     }
 }
