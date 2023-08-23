b8d9ee5b3ac1d3981de0187c07fd2d952524d38c
Roman <roman.zuchowski@gmail.com>
2023-08-23 21:20:08 +0200

    this will add a new tab type

--- Open Charge Map/Models/Tab.swift
+++ Open Charge Map/Models/Tab.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+enum Tab {
+    case Home
+    case Map
+    case More
+    var title: String {
+        switch self {
+        case .Home:
+            return "Home"
+        case .Map:
+            return ""
+        case .More:
+            return "More"
+        }
+    }
+}
