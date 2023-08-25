9b2be4b7e478cdc80142bd2a95ebfb42d12714aa
Roman <roman.zuchowski@gmail.com>
2023-08-25 13:48:39 +0200

    this will add the view model
    this will add the fetchLocation() method for nearby locations

--- Open Charge Map/Views/Home/HomeViewModel.swift
+++ Open Charge Map/Views/Home/HomeViewModel.swift
@@ -6,7 +6,24 @@
 //
 
 import Foundation
+import MapKit
 
 class HomeViewModel: ObservableObject {
+    private let apiService: APIService = APIService.shared
+    private let locationService: LocationService = LocationService.shared
+    @Published var loadingProxResults: Bool = false
+    @Published var locationProxResult: Result<[Location], APIError> = .failure(.unknown("Unbekannter Fehler"))
     
+    func fetchLocations() {
+        self.loadingProxResults = true
+        if let userLocation = locationService.getCoordinates() {
+            let userRegion = MKCoordinateRegion(center: userLocation, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)) // the span will be a fixed value for now
+            apiService.fetchLocations(for: userRegion) { result in
+                DispatchQueue.main.async {
+                    self.loadingProxResults = false
+                    self.locationProxResult = result
+                }
+            }
+        }
+    }
 }
