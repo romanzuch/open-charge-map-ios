4704ea42654186139a8580a5179a85bebc5efca6
Roman <roman.zuchowski@gmail.com>
2023-08-25 17:44:33 +0200

    this will add the boilerplate error view

---
+++ Open Charge Map/Components/ErrorView.swift
@@ -0,0 +1,20 @@
+//
+//  ErrorView.swift
+//  Open Charge Map
+//
+//  Created by Roman Zuchowski on 25.08.23.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ErrorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ErrorView()
+    }
+}
