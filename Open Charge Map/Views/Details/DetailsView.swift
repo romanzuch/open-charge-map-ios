d003eacf1e644b5b013df5772d60f35fe4262c3f
Roman <roman.zuchowski@gmail.com>
2023-08-29 14:38:49 +0200

    this will complete the details view

--- Open Charge Map/Views/Details/DetailsView.swift
+++ Open Charge Map/Views/Details/DetailsView.swift
@@ -20,18 +20,20 @@ struct DetailsView: View {
         NavigationStack {
             ScrollView {
                 VStack(alignment: .leading) {
+                    // MARK: - Basic location information
+                    // this includes, number of plugs, type of plugs, address, availability, distance and max. charging power
                     basicDetails
-                        .padding()
-                        .background {
-                            RoundedRectangle(cornerRadius: 8).fill(Color("offprimary"))
-                                .boxShadow()
-                        }
-                        .padding(8)
+                    // MARK: - Location connections
+                    // this includes a list of all connections, their connection type, max. charging power, pricing and availability
                     connections
+                    // MARK: - Additional location information
+                    // this includes important information, service lines, pricing information and many more
                     others
-                        .padding()
+                    // MARK: - Support information
+                    support
                 }
             }
+            .edgesIgnoringSafeArea(.bottom)
             .navigationTitle(location.properties.address.street)
         }
     }
@@ -62,14 +64,19 @@ struct DetailsView: View {
                     .font(.caption)
             }
         }
+        .padding()
+        .background {
+            RoundedRectangle(cornerRadius: 8).fill(Color("offprimary"))
+                .boxShadow()
+        }
+        .padding(8)
     }
     
     var connections: some View {
         VStack(alignment: .leading, spacing: 8) {
             Text("Anschlüsse")
-                .font(.title3)
+                .font(.title2)
                 .fontWeight(.bold)
-                .padding(.horizontal)
             HStack {
                 Spacer()
                 VStack {
@@ -121,20 +128,47 @@ struct DetailsView: View {
                 Spacer()
             }
         }
+        .padding(4)
     }
     
     var others: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: 8) {
             Text("Weiteres")
-                .font(.title3)
+                .font(.title2)
                 .fontWeight(.bold)
+            operatorInfo
+            Divider()
+            
+            Text("Gebühren und Hinweise").fontWeight(.semibold)
+                .padding(.top, 8)
+            
+            HStack {
+                Image(ChargePointConnector.type2.icon)
+                    .resizable()
+                    .padding(.horizontal, 4)
+                    .frame(width: 56, height: 48, alignment: .center)
+                Spacer()
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Type 2")
+                        .fontWeight(.bold)
+                    Text("Preis je AC kWh: 0,57€ mit Blockiergebühr*")
+                        .font(.footnote)
+                }
+            }
+            .padding()
+            .background {
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(Color("offprimary"))
+                    .boxShadow()
+            }
+            
             HStack {
                 Image(systemName: "exclamationmark.triangle.fill")
                     .renderingMode(.template)
                     .foregroundColor(.white)
                 Spacer()
                 Text("Diese Ladestation erlaubt nicht das Beenden von Ladevorgängen über die App.")
-                    .font(.caption)
+                    .font(.footnote)
             }
             .padding()
             .background {
@@ -142,7 +176,86 @@ struct DetailsView: View {
                     .fill(.orange)
                     .boxShadow()
             }
+            
+            HStack {
+                Image(systemName: "info.circle.fill")
+                    .renderingMode(.template)
+                    .foregroundColor(.orange)
+                Spacer().frame(width: 24)
+                Text("Die angezeigten Preise gelten unabhängig von den Betreiber-Informationen bei der Verwendung der App. Alle Preise inkl. gesetzlicher MwSt.")
+                    .font(.footnote)
+            }
+            .padding()
+            
+            HStack {
+                Image(systemName: "info.circle.fill")
+                    .renderingMode(.template)
+                    .foregroundColor(.orange)
+                Spacer().frame(width: 24)
+                VStack(alignment: .leading) {
+                    Text("* Bitte unsere Blockiergebühr beachten:")
+                    Text("DC: Ab 240 Minuten 0,10€ pro Minute")
+                    Text("AC: Ab 240 Minuten 0,10€ pro Minute")
+                    Text("Maximal: bis 12,00€")
+                }.font(.footnote)
+            }
+            .padding(.horizontal)
         }
+        .padding(8)
+    }
+    
+    var operatorInfo: some View {
+        VStack(spacing: 8) {
+            Text("Betreiber").fontWeight(.semibold)
+            Text(location.properties.locationOperator.title)
+        }
+    }
+    
+    var support: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text("Hilfe & Support")
+                .font(.title2)
+                .fontWeight(.bold)
+            Text("Betreiber: \(location.properties.locationOperator.title)")
+                .font(.footnote)
+                .padding(.bottom, 12)
+            HStack {
+                Text("Rufen Sie bei Problemen mit der Ladestation den Betreiber direkt an.")
+                    .font(.footnote)
+                Spacer()
+                Button {
+                    viewModel.callServiceLine(for: location) { result in
+                        switch result {
+                        case .success(let url):
+                            UIApplication.shared.open(url)
+                        case .failure(let error):
+                            debugPrint(error.localizedDescription)
+                            debugPrint(location.properties.locationOperator.phonePrimary)
+                        }
+                    }
+                } label: {
+                    Image(systemName: "phone")
+                }
+                .buttonStyle(.plain)
+                .foregroundColor(.orange)
+                .padding()
+                .background {
+                    Circle().fill(Color("offprimary"))
+                }
+            }
+            .padding(.bottom, 24)
+            HStack {
+                Spacer()
+                viewModel.getOperatorWebsite(for: location)
+                Spacer()
+            }
+        }
+        .padding()
+        .background {
+            Rectangle()
+                .fill(.orange)
+        }
+        .padding(.top)
     }
 }
 
