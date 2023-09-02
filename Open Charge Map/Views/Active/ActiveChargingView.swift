70971f8aa78aaeb4b31bacd2f3f92358e99bdf9c
Roman <roman.zuchowski@gmail.com>
2023-09-02 20:26:08 +0200

    this will add basic information to the active view

--- Open Charge Map/Views/Active/ActiveChargingView.swift
+++ Open Charge Map/Views/Active/ActiveChargingView.swift
@@ -10,28 +10,51 @@ import SwiftUI
 struct ActiveChargingView: View {
     
     var location: Location
+    @StateObject private var viewModel: ActiveChargingViewModel
     
     init(with location: Location) {
         self.location = location
+        self._viewModel = StateObject(wrappedValue: ActiveChargingViewModel(location: location))
     }
     
     var body: some View {
-        VStack(alignment: .center, spacing: 8) {
-            HStack {
-                Text("Aktiver Ladevorgang")
-                    .font(.title)
-                    .fontWeight(.bold)
+        GeometryReader { geo in
+            VStack(alignment: .center, spacing: 8) {
+                HStack {
+                    Text("Aktiver Ladevorgang")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Spacer()
+                }
+                .padding(.bottom, 48)
+                HStack {
+                    InformationPill(icon: "clock", text: "00:25", size: .medium, geo: geo)
+                    VStack {
+                    InformationPill(icon: "bolt", text: "0,57", size: .small, geo: geo)
+                    InformationPill(icon: "eurosign", text: "0,57€", size: .small, geo: geo)
+                    }
+                }
+                if viewModel.isRoamingLocation() {
+                    HStack {
+                        Image(systemName: "exclamationmark.triangle.fill")
+                            .renderingMode(.template)
+                            .foregroundColor(.white)
+                        Spacer()
+                        Text("Bei Roaming-Ladevorgängen erhalten wir unter Umständen keine aktuellen Daten vom Anbieter. Aus diesem Grund können wir den genauen Preis erst nach Ablauf des Ladevorgangs ermitteln.")
+                            .font(.footnote)
+                            .multilineTextAlignment(.leading)
+                    }
+                    .padding()
+                    .background {
+                        RoundedRectangle(cornerRadius: 8)
+                            .fill(.orange)
+                            .boxShadow()
+                    }
+                }
                 Spacer()
             }
-            .padding(.bottom, 48)
-            HStack {
-                InformationPill(icon: "eurosign", text: "0,12€", subTitle: "Geschätzter Preis", size: .small)
-                InformationPill(icon: "clock", text: "00:20", subTitle: "Dauer", size: .small)
-                InformationPill(icon: "eurosign", text: "0,57€", subTitle: "Preis pro kWh", size: .small)
-            }
-            Spacer()
+            .padding()
         }
-        .padding()
     }
 }
 
