f83a628edcfb6b0a9407fab79c570bfbb4e9c459
Roman <roman.zuchowski@gmail.com>
2023-09-02 20:24:13 +0200

    this will add a geo proxy

--- Open Charge Map/Components/InformationPill.swift
+++ Open Charge Map/Components/InformationPill.swift
@@ -12,32 +12,144 @@ struct InformationPill: View {
     var icon: String
     var text: String
     var size: PillSize
-    var subTitle: String
+    var subTitle: String?
+    var geometryProxy: GeometryProxy
+    let length: CGFloat = 32
     
-    init(icon: String, text: String, subTitle: String, size: PillSize) {
+    init(icon: String, text: String, size: PillSize, geo: GeometryProxy) {
+        self.icon = icon
+        self.text = text
+        self.subTitle = nil
+        self.size = size
+        self.geometryProxy = geo
+    }
+    
+    init(icon: String, text: String, subTitle: String, size: PillSize, geo: GeometryProxy) {
         self.icon = icon
         self.text = text
         self.subTitle = subTitle
         self.size = size
+        self.geometryProxy = geo
     }
     
     var body: some View {
-        VStack(alignment: .center, spacing: 8) {
-            Text(subTitle)
-                .font(.footnote)
-                .foregroundColor(.gray)
-            HStack {
-                Image(systemName: icon)
-                    .font(.title3)
-                    .fontWeight(.bold)
-                Spacer().frame(width: 12)
-                Text(text)
+        switch subTitle {
+        case .none:
+            switch size {
+            case .small:
+                HStack {
+                    Image(systemName: icon)
+                        .font(.title3)
+                        .fontWeight(.bold)
+                    Spacer().frame(width: 12)
+                    Text(text)
+                }
+                .padding()
+                .frame(width: geometryProxy.size.width*(1/3)-(12), height: length*2, alignment: .center)
+                .background {
+                    RoundedRectangle(cornerRadius: 8)
+                        .fill(Color("offprimary"))
+                        .boxShadow()
+                }
+            case .medium:
+                HStack {
+                    Image(systemName: icon)
+                        .font(.title3)
+                        .fontWeight(.bold)
+                    Spacer().frame(width: 12)
+                    Text(text)
+                }
+                .padding()
+                .frame(width: geometryProxy.size.width*(2/3)-(3*12), height: length*4+10, alignment: .center)
+                .background {
+                    RoundedRectangle(cornerRadius: 8)
+                        .fill(Color("offprimary"))
+                        .boxShadow()
+                }
+            case .large:
+                HStack {
+                    Image(systemName: icon)
+                        .font(.title3)
+                        .fontWeight(.bold)
+                    Spacer().frame(width: 12)
+                    Text(text)
+                }
+                .padding()
+                .frame(width: length*10, height: length*5, alignment: .center)
+                .background {
+                    RoundedRectangle(cornerRadius: 8)
+                        .fill(Color("offprimary"))
+                        .boxShadow()
+                }
             }
-            .padding()
-            .background {
-                RoundedRectangle(cornerRadius: 8)
-                    .fill(Color("offprimary"))
-                    .vibrantShadow()
+        case .some(let subtitle):
+            switch size {
+            case .small:
+                VStack {
+                    Text(subtitle)
+                        .font(.footnote)
+                        .foregroundColor(.gray)
+                        .padding(.bottom)
+                    HStack {
+                        Image(systemName: icon)
+                            .font(.title3)
+                            .fontWeight(.bold)
+                        Spacer().frame(width: 12)
+                        Text(text)
+                    }
+                }
+                .padding()
+                .frame(width: geometryProxy.size.width*(1/3), height: length*2, alignment: .center)
+                .padding()
+                .background {
+                    RoundedRectangle(cornerRadius: 8)
+                        .fill(Color("offprimary"))
+                        .boxShadow()
+                }
+            case .medium:
+                VStack {
+                    Text(subtitle)
+                        .font(.footnote)
+                        .foregroundColor(.gray)
+                        .padding(.bottom)
+                    HStack {
+                        Image(systemName: icon)
+                            .font(.title3)
+                            .fontWeight(.bold)
+                        Spacer().frame(width: 12)
+                        Text(text)
+                    }
+                }
+                .padding()
+                .frame(width: geometryProxy.size.width*(2/3)-(3*12), height: length*4+10, alignment: .center)
+                .padding()
+                .background {
+                    RoundedRectangle(cornerRadius: 8)
+                        .fill(Color("offprimary"))
+                        .boxShadow()
+                }
+            case .large:
+                VStack {
+                    Text(subtitle)
+                        .font(.footnote)
+                        .foregroundColor(.gray)
+                        .padding(.bottom)
+                    HStack {
+                        Image(systemName: icon)
+                            .font(.title3)
+                            .fontWeight(.bold)
+                        Spacer().frame(width: 12)
+                        Text(text)
+                    }
+                }
+                .padding()
+                .frame(width: geometryProxy.size.width, height: length*5, alignment: .center)
+                .padding()
+                .background {
+                    RoundedRectangle(cornerRadius: 8)
+                        .fill(Color("offprimary"))
+                        .boxShadow()
+                }
             }
         }
     }
@@ -45,7 +157,15 @@ struct InformationPill: View {
 
 struct InformationPill_Previews: PreviewProvider {
     static var previews: some View {
-        InformationPill(icon: "eurosign", text: "12,23€", subTitle: "Geschätzer Preis", size: .small)
+        GeometryReader { geo in
+            InformationPill(icon: "eurosign", text: "12,23€", subTitle: "Klein", size: .small, geo: geo)
+        }
+        GeometryReader { geo in
+            InformationPill(icon: "eurosign", text: "12,23€", subTitle: "Mittel", size: .medium, geo: geo)
+        }
+        GeometryReader { geo in
+            InformationPill(icon: "eurosign", text: "12,23€", subTitle: "Groß", size: .large, geo: geo)
+        }
     }
 }
 
---
+++ Open Charge Map/Views/Active/ActiveChargingViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ActiveChargingViewModel.swift
+//  Open Charge Map
+//
+//  Created by Roman Zuchowski on 02.09.23.
+//
+
+import Foundation
